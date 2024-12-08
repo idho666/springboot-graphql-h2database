bf0b9f5a675cd40fc66e7d0d733d25c2c5cb96b4
hafidh bm <hafidh.mauidho@iconpln.co.id>
2024-12-08 11:17:52 +0700

    add readme h2 and graphql access url

--- src/test/java/com/idho_web/idho_web/IdhoWebApplicationTests.java
+++ src/test/java/com/idho_web/idho_web/IdhoWebApplicationTests.java
@@ -1,5 +1,6 @@
 package com.idho_web.idho_web;
 
+import org.example.generate.GenerateCaptcha;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,15 @@ class IdhoWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Test
+	void dependencytest() {
+
+		GenerateCaptcha generateCaptcha = new GenerateCaptcha();
+
+		System.out.println(generateCaptcha.valeCaptcha());
 	}
 
 }
